0af7c3282859316cec5a309e82f3c7f623db324f
Rebxxt <allrebxxt@gmail.com>
2024-02-25 05:41:20 +0300

    upgrade and test gulp

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 This project is a draft of future interactive maps for some purposes
 
 # Get Started
->gulp
+### Step 1
+Install dependencies to /node_modules
+```shell
+npm i
+```
+### Step 2
+Run project
+```shell
+gulp
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,24 @@
 import { deleteAsync } from 'del';
 import gulp from 'gulp';
 import gulpSass from 'gulp-sass'
+import imagemin from 'gulp-imagemin';
 import dartSass from 'sass';
 import rename from 'gulp-rename'
 import cleanCSS from 'gulp-clean-css'
 import babel from 'gulp-babel'
 import uglify from 'gulp-uglify'
 import concat from 'gulp-concat'
+import sourcemaps from 'gulp-sourcemaps'
+import autoprefixer from 'gulp-autoprefixer'
+import htmlmin from 'gulp-htmlmin'
 
 const sass = gulpSass(dartSass);
 
 const paths = {
+    html: {
+        src: 'src/**/*.html',
+        dest: 'dist'
+    },
     styles: {
         src: 'src/**/*.scss',
         dest: 'dist/css/'
@@ -18,6 +26,10 @@ const paths = {
     scripts: {
         src: 'src/**/*.js',
         dest: 'dist/js/'
+    },
+    images: {
+        src: 'src/img/*',
+        dest: 'dist/img/'
     }
 }
 
@@ -27,23 +39,38 @@ export function clean() {
 
 export function styles() {
     return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
+        .pipe(autoprefixer({ cascade: false }))
+        .pipe(cleanCSS({ level: 2}))
         .pipe(rename({
             basename: 'main',
             suffix: '.min'
         }))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.styles.dest))
 }
 
 export function scripts() {
-    return gulp.src(paths.scripts.src, {
-        sourcemaps: true
-    })
-        .pipe(babel())
+    return gulp.src(paths.scripts.src)
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['@babel/env'] }))
         .pipe(uglify())
         .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(paths.scripts.dest))
+}
+
+export function img() {
+    return gulp.src(paths.images.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(paths.images.dest))
+}
+
+export function html() {
+    return gulp.src(paths.html.src)
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(paths.html.dest))
 }
 
 export function watch() {
@@ -51,6 +78,6 @@ export function watch() {
     gulp.watch(paths.scripts.src, scripts)
 }
 
-export const build = gulp.series(clean, gulp.parallel(styles, scripts), watch)
+export const build = gulp.series(clean, html, gulp.parallel(styles, scripts, img), watch)
 export default build
 
